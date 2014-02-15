60db6d14595992b01596350eeb5560552d0d366d
Benoit Marcot <juz4m@hotmail.com>
2014-02-15 12:22:31 +0000

    rails new dynboxes --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dynboxes::Application.config.secret_key_base = '88f1579fcbdf45eacda12f5cdaf77b6b38099e3c2376b2ae5f144e38d0cc1ce101604d039a280a3a57a31d0b1f723e939d085f9c7c4e4729fd19bf518eea5b0e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dynboxes::Application.config.session_store :cookie_store, key: '_dynboxes_session'
