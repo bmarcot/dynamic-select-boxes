19d3813dce5b403b929635ec198548a6af66a965
Benoit Marcot <juz4m@hotmail.com>
2014-02-15 16:25:52 +0000

    form with submit feature

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,10 @@ class WelcomeController < ApplicationController
     @cities = City.where("country_id = ?", Country.first.id)
   end
 
+  def show
+    @city = City.find_by("id = ?", params[:trip][:city_id])
+  end
+
   def update_cities
     @cities = City.where("country_id = ?", params[:country_id])
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Dynboxes::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'welcome/update_cities', as: 'update_cities'
+  get 'welcome/show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
