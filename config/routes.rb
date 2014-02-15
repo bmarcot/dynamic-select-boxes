4ef15b5d40011f43e24be4aae26304fadb790aa9
Benoit Marcot <juz4m@hotmail.com>
2014-02-15 12:27:25 +0000

    welcome view and contoller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Dynboxes::Application.routes.draw do
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
