bece77735c666c66fd49bc7275984f6fc3394ce3
Benoit Marcot <juz4m@hotmail.com>
2014-02-15 12:53:53 +0000

    create database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Country.create(name: 'france')
+Country.create(name: 'italy')
+Country.create(name: 'spain')
+
+City.create(name: 'paris', country_id: Country.find_by(name: 'france').id)
+City.create(name: 'nice', country_id: Country.find_by(name: 'france').id)
+City.create(name: 'brest', country_id: Country.find_by(name: 'france').id)
+City.create(name: 'aurillac', country_id: Country.find_by(name: 'france').id)
+
+City.create(name: 'roma', country_id: Country.find_by(name: 'italy').id)
+City.create(name: 'venezia', country_id: Country.find_by(name: 'italy').id)
+City.create(name: 'milano', country_id: Country.find_by(name: 'italy').id)
+City.create(name: 'bari', country_id: Country.find_by(name: 'italy').id)
+
+City.create(name: 'madrid', country_id: Country.find_by(name: 'spain').id)
+City.create(name: 'bilbao', country_id: Country.find_by(name: 'spain').id)
+City.create(name: 'jerez', country_id: Country.find_by(name: 'spain').id)
